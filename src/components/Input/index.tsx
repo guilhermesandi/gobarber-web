01de87fd236fb5b2fe7777f6f46d968a75ee09cc
Guilherme Sandi <guilherme_sandi@hotmail.com>
2020-12-21 17:51:25 -0300

    Fix name form

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -51,7 +51,6 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         ref={inputRef}
         {...rest}
       />
-
       {error}
     </Container>
   );
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -47,7 +47,7 @@ const SignUp: React.FC = () => {
         <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
-          <Input name="nome" icon={FiUser} placeholder="Nome" />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
 
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
