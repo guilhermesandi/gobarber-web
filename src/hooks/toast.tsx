a8a9a9591087dc9c5e9ecd89cfc5064d7a31a350
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-01-05 20:14:08 -0300

    -

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -30,7 +30,7 @@ function useToast(): ToastContextData {
   const context = useContext(ToastContext);
 
   if (!context) {
-    throw new Error('useToast must be used within a ToastProvider');
+    throw new Error('useToast must be used within an ToastProvider');
   }
 
   return context;
